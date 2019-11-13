47b76ab91a64bcf2fd2028f426dfd2c35df3d5af
Sssmeb <351108248@qq.com>
2019-11-13 10:04:05 +0800

    成交订单 redis状态

--- seckilling/mg/utils.py
+++ seckilling/mg/utils.py
@@ -4,7 +4,7 @@
 # @File    : utils.py
 # @Software: PyCharm
 # @Python3.6
-from status import enter_overtime
+from status import enter_overtime, paid_order
 from storage import storage_update_order, storage_insert_order, storage_update_storage
 import json
 import time
@@ -36,7 +36,7 @@ def insert_order(ch, method, properties, body):
 
 def update_storage(ch, method, properties, body):
     body = json.loads(body)
-    if storage_update_storage(body) and storage_update_order(body, 1):
+    if storage_update_storage(body) and storage_update_order(body, 1) and paid_order(body):
         ch.basic_ack(delivery_tag=method.delivery_tag)
     else:
         ch.basic_nack(delivery_tag=method.delivery_tag)
